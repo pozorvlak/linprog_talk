806d2d8b66559f980498d4e3f122a535ad88b212
Miles Gould <miles.gould@hogarthww.com>
2015-05-20 21:00:16 +0100

    Election script: fix number of MPs, improve output

--- election/election.py
+++ election/election.py
@@ -4,25 +4,24 @@ from pulp import *
 import csv
 
 votes = {}
+actual_seats = {}
 total_votes = 0.0
 with open('election.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         score = int(row["Total votes"])
         votes[row["Party"]] = score
+        actual_seats[row["Party"]] = row["Total MPs"]
         total_votes += score
 
 expected = {party: 650*score/total_votes for party, score in votes.items()}
-
-for party, score in expected.items():
-    print("{} => {}".format(party, score))
 
 prob = LpProblem("UK general election 2015", LpMinimize)
 
 # We minimize sum(abs(seats - expected_seats)) using a trick described at
 # http://lpsolve.sourceforge.net/5.5/absolute.htm
 bounds = []
-seat_vars = []
+seat_vars = {}
 for party in votes:
     seats = LpVariable("{}_seats".format(party), 0, 650, LpInteger)
     bound = LpVariable("{}_bound".format(party), 0, 650)
@@ -30,13 +29,18 @@ for party in votes:
     prob += diff <= bound
     prob += -diff <= bound
     bounds.append(bound)
-    seat_vars.append(seats)
+    seat_vars[party] = seats
 
+prob += lpSum(seat_vars.values()) == 650
 prob += lpSum(bounds)
 
 prob.solve()
 
 print("Status:", LpStatus[prob.status])
-results = [(v.name, v.varValue) for v in seat_vars]
-print(sorted(results, key=lambda x:-x[1]))
+results = [(party, v.varValue) for party, v in seat_vars.items()]
+results.sort(key=lambda x:-x[1])
+for result in results:
+    party = result[0]
+    print("{:.2f}\t{}\t{}\t{}".format(
+        expected[party], int(result[1]), actual_seats[party], party))
 print("Error = ", value(prob.objective))
